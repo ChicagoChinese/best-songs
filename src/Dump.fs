bf0764f9c1817de58ba46f4052c2f40ffdc7343e
Feihong Hsu <feihong.hsu@gmail.com>
2020-12-06 15:19:12 -0600

    Reformat .fs files after installing Ionide

--- src/Dump.fs
+++ src/Dump.fs
@@ -4,7 +4,8 @@ open System.IO
 open FSharp.Data
 open Prelude
 
-type FfprobeMeta = JsonProvider<"""
+type FfprobeMeta =
+    JsonProvider<"""
 {
   "format": {
     "tags": {
@@ -18,44 +19,50 @@ type FfprobeMeta = JsonProvider<"""
 }""">
 
 let getTrackLocations playlistName =
-  let output = getCommandOutput "swift" ["track_paths.swift"; playlistName]
-  output.Split "\n"
+    let output =
+        getCommandOutput "swift" [ "track_paths.swift"; playlistName ]
+
+    output.Split "\n"
 
 let getTrackMetaJson location =
-  getCommandOutput "ffprobe" [location; "-print_format"; "json"; "-show_format"]
+    getCommandOutput
+        "ffprobe"
+        [ location
+          "-print_format"
+          "json"
+          "-show_format" ]
 
 let getTrackMeta location =
-  getTrackMetaJson location |> FfprobeMeta.Parse
+    getTrackMetaJson location |> FfprobeMeta.Parse
 
 let parseLink (text: string) =
-  let lines = text.Split "\n"
-  match lines.Length with
-  | 0 -> Track.None
-  | _ ->
-    match lines |> Array.tryFind (fun line -> line.StartsWith "https://youtu.be") with
-    | Some link -> Track.YouTube link
-    | None ->
-      match lines |> Array.tryFind (fun line -> line.StartsWith "https://www.youtube.com") with
-      | Some link -> Track.YouTubeLong link
-      | None -> Track.Other (lines |> Array.tryHead |> Option.defaultValue "")
+    let lines = text.Split "\n"
+    match lines.Length with
+    | 0 -> Track.None
+    | _ ->
+        match lines
+              |> Array.tryFind (fun line -> line.StartsWith "https://youtu.be") with
+        | Some link -> Track.YouTube link
+        | None ->
+            match lines
+                  |> Array.tryFind (fun line -> line.StartsWith "https://www.youtube.com") with
+            | Some link -> Track.YouTubeLong link
+            | None -> Track.Other(lines |> Array.tryHead |> Option.defaultValue "")
 
 let getTrack location =
-  let meta = getTrackMeta location
-  let tags = meta.Format.Tags
-  {
-    location = location
-    title = tags.Title.ToString()
-    artist = tags.Artist.ToString()
-    genre = tags.Genre.ToString()
-    lyrics = tags.Lyrics.ToString()
-    link = parseLink (tags.Comment.ToString())
-  } : Track.T
+    let meta = getTrackMeta location
+    let tags = meta.Format.Tags
+    { location = location
+      title = tags.Title.ToString()
+      artist = tags.Artist.ToString()
+      genre = tags.Genre.ToString()
+      lyrics = tags.Lyrics.ToString()
+      link = parseLink (tags.Comment.ToString()) }: Track.T
 
 let main playlistName =
-  printfn "Dumping track metadata for playlist '%s'\n" playlistName
-  getTrackLocations playlistName
-  |> Array.map getTrack
-  |> Track.serializeTracks
-  |> fun json -> File.WriteAllText("tracks.json", json)
+    printfn "Dumping track metadata for playlist '%s'\n" playlistName
+    getTrackLocations playlistName
+    |> Array.map getTrack
+    |> Track.writeTracksToFile
 
-  printfn "Generated tracks.json"
+    printfn "Generated %s" Track.filename
--- src/Prelude.fs
+++ src/Prelude.fs
@@ -1,34 +1,37 @@
 module Prelude
 
+open System.IO
 open Fake.Core
 open FSharp.Json
 
 let getCommandOutput command args =
-  let result =
-    CreateProcess.fromRawCommand command args
-    |> CreateProcess.redirectOutput
-    |> CreateProcess.ensureExitCode
-    |> Proc.run
-  result.Result.Output.Trim()
+    let result =
+        CreateProcess.fromRawCommand command args
+        |> CreateProcess.redirectOutput
+        |> CreateProcess.ensureExitCode
+        |> Proc.run
+
+    result.Result.Output.Trim()
 
 module Track =
-  type Link =
-    | YouTube of string
-    | YouTubeLong of string
-    | Other of string
-    | None
+    let filename = "tracks.json"
 
-  type T = {
-    title: string
-    artist: string
-    link: Link
-    genre: string
-    lyrics: string
-    location: string
-  }
+    type Link =
+        | YouTube of string
+        | YouTubeLong of string
+        | Other of string
+        | None
 
-  let parse json =
-    Json.deserialize<T> json
+    type T =
+        { title: string
+          artist: string
+          link: Link
+          genre: string
+          lyrics: string
+          location: string }
 
-  let serializeTracks (tracks: T array) =
-    Json.serialize tracks
+    let readTracksFromFile json = Json.deserialize<T array> json
+
+    let writeTracksToFile (tracks: T array) =
+        let json = Json.serialize tracks
+        File.WriteAllText(filename, json)
