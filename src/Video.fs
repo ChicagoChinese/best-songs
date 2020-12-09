dd0aeb8f2744610fbc6b5a45e03d991606bf09bb
Feihong Hsu <feihong.hsu@gmail.com>
2020-12-09 12:55:14 -0600

    Extract artwork from m4a file

--- src/Video.fs
+++ src/Video.fs
@@ -1,6 +1,27 @@
 module Video
 
+open System.IO
+open Fake.Core
 open Prelude
+
+// https://github.com/feihong/music-tools/blob/master/tasks/convert.py#L47
+
+let assetsDir = "assets"
+
+let extractImage trackFile =
+    CreateProcess.fromRawCommand "AtomicParsley" [ trackFile; "--extractPix" ]
+    |> CreateProcess.redirectOutput
+    |> CreateProcess.ensureExitCode
+    |> Proc.run
+    |> ignore
+
+    let imageFile =
+        Path.Combine
+            (assetsDir,
+             Path.GetFileNameWithoutExtension(trackFile)
+             + "_artwork_1.jpg")
+
+    if File.Exists(imageFile) then Some imageFile else None
 
 let main limit =
     let tracks =
@@ -10,3 +31,10 @@ let main limit =
     let tracks = tracks.[0..(limit - 1)]
     for track in tracks do
         printfn "%s" track.Title
+
+        let destFile =
+            Path.Combine(assetsDir, Path.GetFileName(track.Location))
+
+        File.Copy(track.Location, destFile, true)
+
+        printfn "%A" (extractImage destFile)
