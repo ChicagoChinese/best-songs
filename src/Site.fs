242eba3ca3a416fdf801855d65816fcb56fd2666
Feihong Hsu <feihong.hsu@gmail.com>
2020-12-19 17:41:19 -0600

    Generate nice slugs for track titles

--- src/Site.fs
+++ src/Site.fs
@@ -1,7 +1,22 @@
 module Site
 
+open JiebaNet.Segmenter
+open ToolGood.Words.Pinyin
 open Prelude
 
 let main () =
+    let segmenter = JiebaSegmenter()
+
     for playlist in Playlist.getPlaylists () do
         printfn $"{playlist.Name}"
+
+        for track in playlist.Tracks do
+            let segments =
+                segmenter.Cut(track.Title, cutAll = true)
+
+            printfn
+                "%s"
+                (segments
+                 |> Seq.filter (fun s -> s <> "")
+                 |> Seq.map (fun s -> WordsHelper.GetPinyin(s).ToLower())
+                 |> String.concat "-")
